ad3c1c832a398fe5aac0846f231d59d54859a135
Nathan Adcock <nathan.adcock43@gmail.com>
2021-04-26 15:02:04 -0500

    Feat: control flow exercises finished question do while to 1 mill

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,25 +1,50 @@
 import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.println("i is " + i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println("i is " + i);
+//            i++;
+//        }
 
-        int y = 0;
+//        int y = 0;
+//        do {
+//            y++;
+//            if (y%2 == 0) {
+//                System.out.println("y is equal to:");
+//                System.out.println(y);
+//            }
+//
+//        } while (y <= 100);
+//
+//
+//    }
+//        for (y =0; y<=)
+
+//    int y = 100;
+//        do {
+//        y-=5;
+//            System.out.println("y is equal to:");
+//            System.out.println(y);
+//    } while (y > -10);
+//    }
+
+        long y = 2;
+
         do {
-            y++;
-            if (y%2 == 0) {
-                System.out.println("y is equal to:");
-                System.out.println(y);
-            }
-
-        } while (y <= 100);
-
-
-    }
+        y*=y;
+            System.out.print("y is equal to: ");
+            System.out.println(y);
+    } while (y < 65535);
 }
+
+
+
+}
+
+
+
+
 
 
 
