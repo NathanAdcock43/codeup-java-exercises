2d54f65d403b185089a2f22e8ab09e825ebae3cd
Nathan Adcock <nathan.adcock43@gmail.com>
2021-04-26 15:47:29 -0500

    Feat: control flow exercises finished section 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -29,13 +29,49 @@ public class ControlFlowExercises {
 //    } while (y > -10);
 //    }
 
-        long y = 2;
+//        long y = 2;
+//
+//        do {
+//        y*=y;
+//            System.out.print("y is equal to: ");
+//            System.out.println(y);
+//    } while (y < 65535);
 
+//        long y;
+//        for (y= 2; y < 1000000; y*=y){
+//            System.out.print("y is equal to: ");
+//            System.out.println(y);
+//        }
+//
+//        long y = 1;
+//        do {
+//            y++;
+//            if (y%5 == 0 && y%3 == 0) {
+//                System.out.println("FizzBuzz");
+//            }else if (y%3 == 0) {
+//                System.out.println("Fizz");
+//            }else if (y%5 == 0) {
+//                System.out.println("Buzz");
+//            }while (y < 99)
+//                System.out.println(y);
+//        }
+
+        Scanner userInt = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int userInput = userInt.nextInt();
+
+    int y = userInput;
         do {
-        y*=y;
-            System.out.print("y is equal to: ");
+        y++;
+            System.out.println("number");
             System.out.println(y);
-    } while (y < 65535);
+    } while (y > -10);
+    }
+
+
+        System.out.printf("%s!", userInput);
+
+
 }
 
 
