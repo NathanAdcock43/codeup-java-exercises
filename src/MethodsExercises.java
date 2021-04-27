d557de2e526f22faa2c225662aa3bf4950d4c7c5
Nathan Adcock <nathan.adcock43@gmail.com>
2021-04-27 14:45:50 -0500

    Feat: question 1 problem started bonus

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,5 +1,32 @@
 public class MethodsExercises {
         public static void main(String[] args) {
-
+                addition(5, 5);
+                subtraction(3, 2);
+                multiplication(2, 2);
+                division(4, 2);
         }
+
+        public static void addition(double add1, double add2) {
+                double result = add1 + add2;
+                System.out.println(result);
+        }
+
+        public static double subtraction(double sub1, double sub2) {
+                double result = sub1 + sub2;
+                System.out.println(result);
+        }
+
+        public static double multiplication(double mult1, double mult2) {
+                result = mult1 * mult2;
+                return result;
+                System.out.println(result);
+        }
+
+        public static int division(int div1, int div2) {
+                return result = div1 / div2;
+                return result;
+                System.out.println(result);
+        }
+
+
 }
