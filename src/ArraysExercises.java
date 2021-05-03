3ea6534561e7fe0b5fd4483a1520edea7352c55d
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-03 16:33:38 -0500

    feat: Java Arrays exercises question 2 confused

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class ArraysExercises {
@@ -15,15 +16,23 @@ public class ArraysExercises {
 
         System.out.println("Arrays.toString(people) = " + Arrays.toString(people));
 
+        System.out.println();
+
         for (String person: people){
 
             System.out.println("person = " + person + " was a member of a comedy troop");
         }
-//        System.out.println();
-//        String[] newList = Arrays.copyOf(people, 3);
+
+        System.out.println();
+        String troopComedian1 = "Abbott";
+        System.out.println(Arrays.toString(addPerson(people, troopComedian1)));
 
     }
 
+    static String[] addPerson(String[] people, String name) {
+        String[] additionalPeople = Arrays.copyOf(people, (people.length + 1));
+        additionalPeople[people.length + 1] = name ;
 
-
+        return additionalPeople;
+    }
 }
