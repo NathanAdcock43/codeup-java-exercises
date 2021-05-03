fffc989516c3bd1be7d6534f4562359df4b7c704
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-03 12:30:08 -0500

    feat: OOP exercise part 3 not done

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,34 +1,70 @@
 package util;
 import java.util.Scanner;
 public class Input {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
+        System.out.println(userInput.getString());
+        System.out.println(userInput.yesNo());
+        System.out.println(userInput.getInt());
+        System.out.println(userInput.getDouble());
+        System.out.println(userInput.getString());
+        System.out.println(userInput.getString());
     }
-    Scanner scanner = new Scanner (System.in);
-        String userReady = scanner.next();
 
+        System.out.print("Enter an integer: ");
+        int userInput = scanner.nextInt();
    public String getString(){
-
+        if (typeof(this) == String){
+            return true;
+        }else {
+       return false;
     }
-
+    }
+        System.out.print("Enter an integer: ");
+        int userInput = scanner.nextInt();
     public boolean yesNo(){
-
+        if (this.equalsIgnoreCase("y") || this.equalsIgnoreCase("yes")){
+            return true;
+        }else {
+            return false;
+        }
     }
-
-    public int getInt(int min, int max){
-
-    }
-
+        System.out.print("Enter an integer: ");
+        int userInput = scanner.nextInt();
+//    public int getInt(int min, int max){
+//        if (typeof(this) == int){
+//            return true;
+//        }else {
+//            return false;
+//        }
+//    }
+        System.out.print("Enter an integer: ");
+        int userInput = scanner.nextInt();
     public int getInt(){
-
+        if (typeof(this) == int){
+            return true;
+        }else {
+            return false;
+        }
     }
-
-    public double getDouble(double min, double max){
-
-    }
-
+        System.out.print("Enter an integer: ");
+        int userInput = scanner.nextInt();
+//    public double getDouble(double min, double max){
+//        if (typeof(this) == double){
+//            return true;
+//        }else {
+//            return false;
+//        }
+//    }
+        System.out.print("Enter an integer: ");
+        int userInput = scanner.nextInt();
     public double getDouble(){
-
+        if (typeof(this) == double){
+            return true;
+        }else {
+            return false;
+        }
     }
 
 }
