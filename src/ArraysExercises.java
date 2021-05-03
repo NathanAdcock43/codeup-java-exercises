5ba7398b9667aeb275b58b34b7bf392006d58e99
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-03 16:14:05 -0500

    feat: Java Arrays exercises question 1 done

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -6,6 +6,22 @@ public class ArraysExercises {
     public static void main (String[] arg){
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
+
+        String[] people = new String[3];
+
+        people[0] = "Larry";
+        people[1] = "Curly";
+        people[2] = "Moe";
+
+        System.out.println("Arrays.toString(people) = " + Arrays.toString(people));
+
+        for (String person: people){
+
+            System.out.println("person = " + person + " was a member of a comedy troop");
+        }
+//        System.out.println();
+//        String[] newList = Arrays.copyOf(people, 3);
+
     }
 
 
