fb4f755ed5fa2dd5f4706866bc0ecfbd0debc9e1
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-03 11:54:54 -0500

    feat: OOP exercise part 2 done

--- src/Person.java
+++ src/Person.java
@@ -4,30 +4,30 @@ public class Person {
 
         Person nathan = new Person();
         nathan.name = "Nathan";
-        System.out.println(nathan.getName());
-        System.out.println(nathan.setName("Nate"));
+        nathan.getName();
+        nathan.setName("Nate");
         nathan.sayHello();
 
         Person person1 = new Person();
         Person person2 = person1;
         person1.name = "John";
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
+        person1.getName();
+        person2.getName();
         person2.setName("Jane");
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
+        person1.getName();
+        person2.getName();
     }
     private String name;
 
-    public String getName() {
+    public void getName() {
 //TODO: return the person's name
-        return name;
+        System.out.println(name);
     }
 
-    public String setName(String name) {
+    public void setName(String name) {
 //TODO: change the name property to the passed value
        this.name = name;
-       return name;
+        System.out.println(this.name);
     }
 
     public void sayHello() {
---
+++ src/util/Input.java
@@ -0,0 +1,9 @@
+package util;
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+
+    }
+//    Scanner scanner = new scanner (System.in);
+
+}
