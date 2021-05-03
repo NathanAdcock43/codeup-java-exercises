daa29911b035600ec0bc0efd7d6395cfe3123a98
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-03 09:32:03 -0500

    feat: person exercise 1

---
+++ src/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    public static void main(String[] args) {
+       getName();
+
+    }
+    private String name;
+
+    public String getName() {
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name) {
+//TODO: change the name property to the passed value
+
+    }
+
+    public void sayHello() {
+//TODO: print a message to the console using the person's name
+        System.out.println(name);
+    }
+}
