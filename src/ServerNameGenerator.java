9f86ee26eefe0674079070093af6a5e5b0730c25
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-04 11:18:04 -0500

    feat: Java Arrays exercises question 1 refactored back to question 2

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,9 @@
 public class ServerNameGenerator {
+    public static void main(String [] args){
+        String[] nouns = {};
+        String[] adjectives = {};
+        System.out.println("Here is your server name: ");
+
+
+    }
 }
