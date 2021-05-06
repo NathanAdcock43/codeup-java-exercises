b9d6d79d9b09bad4a9af5407eb24a40818848d8b
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-06 09:07:14 -0500

    feat: Java StringExercise finished Bob Question refactor

--- src/Bob.java
+++ src/Bob.java
@@ -7,37 +7,37 @@ public class Bob {
     }
 
 
-    public static void Bob() {
-
-
-        do {
-            Scanner bob = new Scanner(System.in);
-            System.out.println("Would you like to speak with Bob?");
-            String UserInput1 = bob.nextLine();
-            if (UserInput1.equalsIgnoreCase("yes")){
-
-            }else{
-
-            }
-
-
-            String UserInput2 = bob.next();
-            String bobResponseQuestion = "Bob: Sure";
-            String bobResponseExclaim = "Bob: Whoa, chill out!";
-            String bobResponseEmpty = "Bob: Fine. Be that way!";
-            String bobResponseElse = "Bob: Whatever.";
-
-
-            if (UserInput2.contains("?")) {
-                System.out.println(bobResponseQuestion);
-            } else if (UserInput2.contains("!")) {
-                System.out.println(bobResponseExclaim);
-            } else if (UserInput2.equals("")) {
-                System.out.println(bobResponseEmpty);
-            } else {
-                System.out.println(bobResponseElse);
-            }
-
-        } while ()
-    }
+//    public static void Bob() {
+//
+//
+//        do {
+//            Scanner bob = new Scanner(System.in);
+//            System.out.println("Would you like to speak with Bob?");
+//            String UserInput1 = bob.nextLine();
+//            if (UserInput1.equalsIgnoreCase("yes")){
+//
+//            }else{
+//
+//            }
+//
+//
+//            String UserInput2 = bob.next();
+//            String bobResponseQuestion = "Bob: Sure";
+//            String bobResponseExclaim = "Bob: Whoa, chill out!";
+//            String bobResponseEmpty = "Bob: Fine. Be that way!";
+//            String bobResponseElse = "Bob: Whatever.";
+//
+//
+//            if (UserInput2.contains("?")) {
+//                System.out.println(bobResponseQuestion);
+//            } else if (UserInput2.contains("!")) {
+//                System.out.println(bobResponseExclaim);
+//            } else if (UserInput2.equals("")) {
+//                System.out.println(bobResponseEmpty);
+//            } else {
+//                System.out.println(bobResponseElse);
+//            }
+//
+//        } while ()
+//    }
 }
