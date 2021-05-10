c8ffbb46f3625818541300a5a503be32eb28eaff
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-10 14:41:23 -0500

    feat: Java Input, fixed and question 1 done on Java IO exercise done

--- src/util/Input.java
+++ src/util/Input.java
@@ -4,32 +4,29 @@ import java.util.Scanner;
 import java.lang.reflect.Field;
 
 public class Input {
-    static Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
 //        System.out.println(userInput.getString());
 //        System.out.println(userInput.yesNo());
-        System.out.println(getInt());
+            Input in = new Input();
+            System.out.println("Input.getInt(1, 10) = " + in.getInt(1, 10));
 //        System.out.println(userInput.getDouble());
 //        System.out.println(userInput.getString());
 //        System.out.println(userInput.getString());
     }
 
-    public Input(Double number){
-        System.out.print("Enter an integer: ");
-        int userInput = scanner.nextInt();
-        this.userInput = number;
+
+
+
+
+   public String getString(){
+       System.out.print("Enter an integer: ");
+       String userInput = scanner.next();
+        return this.scanner.nextLine();
     }
 
-//        System.out.print("Enter an integer: ");
-//        int userInput = scanner.nextInt();
-//   public String getString(){
-//        if (typeof(this) == String){
-//            return true;
-//        }else {
-//       return false;
-//    }
-//    }
 //        System.out.print("Enter an integer: ");
 //        int userInput = scanner.nextInt();
 //    public boolean yesNo(){
@@ -39,14 +36,20 @@ public class Input {
 //            return false;
 //        }
 //    }
+//Integer.valueOf(this)
 
-    public static int getInt(int min, int max){
-        System.out.print("Enter an integer: ");
-        int userInput = scanner.nextInt();
-        if (userInput.getType() == int){
-            return true;
-        }else {
-            return false;
+    public int getInt(int min, int max){
+        try {
+            System.out.print("Enter an integer: ");
+            int userInput = Integer.valueOf(this.getString());
+            if (userInput < max && userInput > min) {
+                return userInput;
+            } else {
+                return getInt(min, max);
+            }
+        }catch(Exception e){
+            System.out.println("Try Again");
+            return getInt(min, max);
         }
     }
 //        System.out.print("Enter an integer: ");
@@ -58,15 +61,16 @@ public class Input {
 //            return false;
 //        }
 //    }
-//        System.out.print("Enter an integer: ");
-//        int userInput = scanner.nextInt();
-//    public double getDouble(double min, double max){
-//        if (typeof(this) == double){
-//            return true;
-//        }else {
-//            return false;
-//        }
-//    }
+
+    public double getDouble(double min, double max){
+        System.out.print("Enter a Double: ");
+        int userInput = scanner.nextInt();
+        if (userInput < max && userInput > min){
+            return userInput;
+        }else {
+            return getDouble(min, max);
+        }
+    }
 //        System.out.print("Enter an integer: ");
 //        int userInput = scanner.nextInt();
 //    public double getDouble(){
